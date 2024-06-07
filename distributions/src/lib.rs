ad9e004dbfb87543ad5f84f9fbc41a8e1eff2ecf
Johannes <jojurg@ttu.ee>
2024-06-07 17:30:44 +0300

    statistical distributions vol 2

--- distributions/src/lib.rs
+++ distributions/src/lib.rs
@@ -9,20 +9,18 @@ mod beta_distribution;
 trait ProbabilityDistribution {
     type Source;
     fn generate_random_sample(&self, source: &mut Self::Source) -> f64;
-    fn generate_random_pair(&self, source: &mut Self::Source) -> (f64, f64);
+    fn generate_random_pair(&self, source: &mut Self::Source) -> (f64, f64) {
+        let rand1 = self.generate_random_sample(source);
+        let rand2 = self.generate_random_sample(source);
+
+        (rand1, rand2)
+    }
 }
 
 impl ProbabilityDistribution for NormalDistribution {
     type Source = source::Default;
     fn generate_random_sample(&self, source: &mut Self::Source) -> f64 {
         self.dist.sample(source)
-    }
-
-    fn generate_random_pair(&self, source: &mut Self::Source) -> (f64, f64) {
-        let rand1: f64 = self.generate_random_sample(source);
-        let rand2: f64 = self.generate_random_sample(source);
-
-        (rand1, rand2)
     }
 }
 
@@ -31,12 +29,5 @@ impl ProbabilityDistribution for BetaDistribution {
 
     fn generate_random_sample(&self, source: &mut Self::Source) -> f64 {
         self.dist.sample(source)
-    }
-
-    fn generate_random_pair(&self, source: &mut Self::Source) -> (f64, f64) {
-        let rand1: f64 = self.generate_random_sample(source);
-        let rand2: f64 = self.generate_random_sample(source);
-
-        (rand1, rand2)
     }
 }
