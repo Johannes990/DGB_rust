7e71426b674cebe7f8d776699949374def09b738
Johannes990 <122638162+Johannes990@users.noreply.github.com>
2024-06-15 13:56:05 +0300

    some more refactoring

--- distributions/src/distribution_class.rs
+++ distributions/src/distribution_class.rs
@@ -2,32 +2,34 @@ use crate::distribution_type::DistributionType;
 
 #[derive(Clone, PartialEq)]
 pub struct DistributionClass {
-    pub distribution_type: DistributionType,
-    pub distribution_name: String,
+    pub payload: DistributionType,
+    pub name: String,
 }
 
 pub trait HasName {
     fn get_name(&self) -> &str;
 }
 
-pub trait HasDistributionType {
-    fn get_distribution_type(&self) -> DistributionType;
+pub trait HasPayload {
+    type Payload;
+    fn get_payload(&self) -> Self::Payload;
 }
 
 impl HasName for DistributionClass {
     fn get_name(&self) -> &str {
-        &self.distribution_name
+        &self.name
     }
 }
 
-impl HasDistributionType for DistributionClass {
-    fn get_distribution_type(&self) -> DistributionType {
-        self.distribution_type.clone()
+impl HasPayload for DistributionClass {
+    type Payload = DistributionType;
+    fn get_payload(&self) -> &Self::Payload {
+        &self.payload
     }
 }
 
 impl DistributionClass {
     pub fn new(distribution_type: DistributionType, distribution_name: String) -> Self {
-        Self { distribution_type, distribution_name }
+        Self { payload: distribution_type, name: distribution_name }
     }
 }
